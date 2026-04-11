161f5714a3e18d17d2e6f89fee23da8d6a39f460
Joerg Heinemann <heinemannj66@gmail.com>
2026-04-11 07:59:14 +0200

    Update step-admin.sh

--- tools/addon/step-admin.sh
+++ tools/addon/step-admin.sh
@@ -517,7 +517,7 @@ function x509_view(){
   VALID_CERTS=$(grep -c "valid" "$CERT_PATH/x509/x509Certs.txt")
   EXPIRED_CERTS=$(( TOTAL_CERTS - VALID_CERTS ))
   echo -e "\n" >> "$CERT_PATH/x509/x509Certs.txt"
-  echo -e "\nTotal Certificates: ${TOTAL_CERTS}\nValid Certificates: ${VALID_CERTS}\nExpired Certificates: ${EXPIRED_CERTS}" >> "$CERT_PATH/x509/x509Certs.txt"
+  echo -e "\nTotal Certificates  : ${TOTAL_CERTS}\nValid Certificates  : ${VALID_CERTS}\nExpired Certificates: ${EXPIRED_CERTS}" >> "$CERT_PATH/x509/x509Certs.txt"
   cat "$CERT_PATH/x509/x509Certs.txt" | column -t -s '|' > "$CERT_PATH/x509/x509Certs.txt"
 }
 
