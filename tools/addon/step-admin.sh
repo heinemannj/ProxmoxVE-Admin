e531e1a204f1e7b89cd6e7b985b5a0e8246fdd34
Joerg Heinemann <heinemannj66@gmail.com>
2026-04-14 12:15:44 +0200

    Update step-admin.sh

--- tools/addon/step-admin.sh
+++ tools/addon/step-admin.sh
@@ -198,10 +198,12 @@ function init_app() {
     PROVISIONER="acme@$(hostname -d)"
   fi
 
-  CA_URL=$(grep -s "ca-url" "$CA_DEFAULTS" | awk -F'"ca-url": "' '{print $2}' | awk -F'"' '{print $1}')
-  [[ -n $CA_URL ]] && CA_FQDN=$(echo "$CA_URL" | awk -F'https://' '{print $2}' | awk -F':' '{print $1}') || CA_FQDN="step-ca.$(hostname -d)"
-  CA_FINGERPRINT=$(grep "fingerprint" "$CA_DEFAULTS" | awk -F'"fingerprint": "' '{print $2}' | awk -F'"' '{print $1}')
-  CA_ROOT=$(grep "root" "$CA_DEFAULTS" | awk -F'"root": "' '{print $2}' | awk -F'"' '{print $1}')
+  if [ -f "$CA_DEFAULTS"]; then
+    CA_URL=$(grep -s "ca-url" "$CA_DEFAULTS" | awk -F'"ca-url": "' '{print $2}' | awk -F'"' '{print $1}')
+    [[ -n $CA_URL ]] && CA_FQDN=$(echo "$CA_URL" | awk -F'https://' '{print $2}' | awk -F':' '{print $1}') || CA_FQDN="step-ca.$(hostname -d)"
+    CA_FINGERPRINT=$(grep "fingerprint" "$CA_DEFAULTS" | awk -F'"fingerprint": "' '{print $2}' | awk -F'"' '{print $1}')
+    CA_ROOT=$(grep "root" "$CA_DEFAULTS" | awk -F'"root": "' '{print $2}' | awk -F'"' '{print $1}')
+  fi
   [ -z "$CA_ROOT" ] && CA_ROOT="${CERT_PATH}/root_ca.crt"
 
   mkdir -p "$CERT_PATH/ssh/_archive/"
