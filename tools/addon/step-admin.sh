efa1b02625c8481ab8bf5b484fb114691af07dba
Joerg Heinemann <heinemannj66@gmail.com>
2026-04-16 17:42:37 +0200

    Update step-admin.sh

--- tools/addon/step-admin.sh
+++ tools/addon/step-admin.sh
@@ -781,6 +781,8 @@ function ssh_maintenance_menu() {
 
 function ca_maintenance_menu() {
   local CHOICE
+  test="Testxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  test="Testxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   OPTIONS=("root" "Inspect Root CA Certificate ($CA_ROOT)"
     "intermediate" "Inspect Intermediate CA Certificate ($CA_CRT)"
     "intermediate-url" "Inspect Intermediate CA Certificate ($CA_URL_CRT)")
