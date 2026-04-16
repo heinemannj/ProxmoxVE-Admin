f99786969bc23f50a0b2f9d8b2f63f11da113cc8
Joerg Heinemann <heinemannj66@gmail.com>
2026-04-16 17:32:59 +0200

    Update step-admin.sh

--- tools/addon/step-admin.sh
+++ tools/addon/step-admin.sh
@@ -783,7 +783,7 @@ function ca_maintenance_menu() {
   local CHOICE
   OPTIONS=("root" "Inspect Root CA Certificate ($CA_ROOT)"
     "intermediate" "Inspect Intermediate CA Certificate ($CA_CRT)"
-    "intermediate-url" "Inspect Intermediate CA Certificate ($CA_URL_CRT)"
+    "intermediate-url" "Inspect Intermediate CA Certificate ($CA_URL_CRT)")
 
   CHOICE=$(whiptail_menu "$APP_TITLE")
   case "$CHOICE" in
