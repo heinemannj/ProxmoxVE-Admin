29f6c13bcaec85ee3fd522bd207e73a60552a1c8
Joerg Heinemann <heinemannj66@gmail.com>
2026-04-20 09:35:58 +0200

    Update step-admin.sh

--- tools/addon/step-admin.sh
+++ tools/addon/step-admin.sh
@@ -432,7 +432,7 @@ function x509_request() {
     --provisioner="$CA_PROVISIONER"
     --set country="DE"
     --set organization="$CA_ORG"
-    --set organizationalUnit="MyHomeLab"
+    --set organizationalUnit=["MyHomeLab"]
 #    --set locality="Schermbeck"
 #    --set province="NRW"
 #    --set streetAddress="Schetterstr. 54B"
