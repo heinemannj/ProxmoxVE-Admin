13e0f12879aba25545fe1ee046a84c72ad1e4678
Joerg Heinemann <heinemannj66@gmail.com>
2026-04-21 08:47:56 +0200

    Update step-admin.sh

--- tools/addon/step-admin.sh
+++ tools/addon/step-admin.sh
@@ -585,10 +585,10 @@ function ca_inspect_intermediate() {
 
 function ca_inspect_intermediate_url() {
   local BACK_TO_MENU="${1:-}"
-  local CERT_VALIDITY=""
-  local CERT_INSPECT=""
-  CERT_VALIDITY=$(step certificate verify --verbose --issuing-ca="$CA_CRT" --crl-endpoint="$CA_URL_CRL" --verify-crl "$CA_URL_CRT")
-  CERT_INSPECT="Certificate path validation:\n\n${CERT_VALIDITY}\n\n"
+  local CERT_INSPECT="Certificate Location:\n\n"
+  CERT_INSPECT+="$CA_URL_CRT\n\n"
+  CERT_INSPECT+="Certificate Path Validation:\n\n"
+  CERT_INSPECT+="$(step certificate verify --verbose --issuing-ca="$CA_CRT" --crl-endpoint="$CA_URL_CRL" --verify-crl "$CA_URL_CRT")\n\n"
   CERT_INSPECT+=$(step certificate inspect "$CA_URL_CRT" --roots="$CA_ROOT" --insecure --bundle)
   whiptail_msgbox "Intermediate CA Certificate ($CA_URL_CRT)" "$CERT_INSPECT"
   [[ "$BACK_TO_MENU" ]] && "$BACK_TO_MENU" || true
