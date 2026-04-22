e53f93f5c927b19a100dd15ca9942811ee320428
Joerg Heinemann <heinemannj66@gmail.com>
2026-04-22 14:15:06 +0200

    Update step-admin.sh

--- tools/addon/step-admin.sh
+++ tools/addon/step-admin.sh
@@ -541,10 +541,10 @@ function ca_renew_intermediate() {
   done
 
 
-  echo "step certificate create $CA_CN_CRT ${CA_CRT} ${CA_CRT_KEY} ${FLAGS[@]}"
+  echo "step certificate create \"${CA_CN_CRT}\" ${CA_CRT} ${CA_CRT_KEY} ${FLAGS[@]}"
 
   $STD echo
-  $STD step certificate create "$CA_CN_CRT" \
+  $STD step certificate create "\"${CA_CN_CRT}\"" \
     "${CA_CRT}" \
     "${CA_CRT_KEY}" \
     "${FLAGS[@]}" || die "Certificate Signing Request (CSR) by $CA_PROVISIONER failed!"
