6e9f18577f0a2366b3418c7fce40df939cc6fb9b
Joerg Heinemann <heinemannj66@gmail.com>
2026-04-22 10:56:52 +0200

    Update step-admin.sh

--- tools/addon/step-admin.sh
+++ tools/addon/step-admin.sh
@@ -450,6 +450,7 @@ function x509_request() {
   #  "${KEY_PATH}"/"$FQDN".key \
   #  "${FLAGS[@]}" || die "Certificate Signing Request (CSR) by $CA_PROVISIONER failed!"
 
+  echo "test"
   echo "step certificate create $FQDN ${CERT_PATH}/x509/$FQDN.crt ${KEY_PATH}/$FQDN.key ${FLAGS[@]}"
 
   $STD echo
