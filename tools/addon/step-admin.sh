19a572d210647ed82c01420e2def8dddb7f53d86
Joerg Heinemann <heinemannj66@gmail.com>
2026-05-05 18:57:28 +0200

    Fix syntax error in step-admin.sh

--- tools/addon/step-admin.sh
+++ tools/addon/step-admin.sh
@@ -455,8 +455,7 @@ function x509_request() {
     --set organization="$CA_ORG"
     --set organizationalUnit="MyHomeLab"
     --set issuingCertificateURL="$CA_URL_CRT"
-    --set crlDistributionPoints="$CA_URL_CRL"
-    --http-listen=5002)
+    --set crlDistributionPoints="$CA_URL_CRL")
   [ "$CA_PROVISIONER_TYPE" = "JWK" ] && [ -f "$CA_PROVISIONER_PWD_FILE" ] && FLAGS+=(--provisioner-password-file="$CA_PROVISIONER_PWD_FILE")
   local SAN_ITEMS=("$FQDN" "$HOST" "$IP" "$SAN")
   for item in "${SAN_ITEMS[@]}"; do
